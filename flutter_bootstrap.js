d6dfb9da21264c344d076ea9ac739c3ee7f8c581
Mamun Or Rashid <64189817+MamunOr@users.noreply.github.com>
2025-05-18 22:41:11 -0230

    Update: fix base href for GitHub Pages deployment

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2987545589"
+    serviceWorkerVersion: "1171273131"
   }
 });
